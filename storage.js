b62135c3dc108094e1b99e4e401e4de8f4f5069d
Davo Ramirez <105182325+davoweb3@users.noreply.github.com>
2023-11-20 22:06:48 -0500

    change input structure bottles and wallet

--- storage.js
+++ storage.js
@@ -37,43 +37,33 @@ wss.on('connection', (ws) => {
 });
 
 app.post('/scan', (req, res) => {
-  const { cart } = req.body;
-  if (cart && Array.isArray(cart)) {
-    cart.forEach(item => {
-      scannedData.push(item);
-      console.log(`Received and stored scanned data: ${JSON.stringify(item)}`);
-    });
+  const { cart, walletAddress } = req.body;
+
+  if (cart && Array.isArray(cart) && walletAddress) {
+    scannedData.push({ cart, walletAddress });
+    console.log(`Received and stored scanned data: ${JSON.stringify({ cart, walletAddress })}`);
 
     if (arraysAreEqual(scannedData, otherScannedData)) {
       console.log('Both scannedData and otherScannedData match.');
-      wss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send('Both scannedData and otherScannedData match.');
-        }
-      });
+      notifyClients();
     }
 
-    res.status(200).json({ message: 'Data received and stored in scannedData.' });
+    res.status(200).json({ message: 'Data received and stored.' });
   } else {
-    res.status(400).json({ message: 'Invalid data received. Expecting an array in the format { "cart": [...] }' });
+    res.status(400).json({ message: 'Invalid data received. Expecting an object with "cart" as an array and "walletAddress" as a string.' });
   }
 });
 
 app.post('/scan2', (req, res) => {
   const { cart } = req.body;
+
   if (cart && Array.isArray(cart)) {
-    cart.forEach(item => {
-      otherScannedData.push(item);
-      console.log(`Received and stored scanned data in otherScannedData: ${JSON.stringify(item)}`);
-    });
+    otherScannedData.push({ cart });
+    console.log(`Received and stored scanned data in otherScannedData: ${JSON.stringify({ cart })}`);
 
     if (arraysAreEqual(scannedData, otherScannedData)) {
       console.log('Both scannedData and otherScannedData match.');
-      wss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send('Both scannedData and otherScannedData match.');
-        }
-      });
+      notifyClients();
     }
 
     res.status(200).json({ message: 'Data received and stored in otherScannedData.' });
@@ -99,3 +89,11 @@ app.get('/scanned-data2', (req, res) => {
 httpServer.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+function notifyClients() {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send('Both scannedData and otherScannedData match.');
+    }
+  });
+}
